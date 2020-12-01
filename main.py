f56571db748a4e2525d303c26ce578772a10c1f2
denis burnashev <kardenman@gmail.com>
2020-12-01 10:11:14 +0600

    v 2.0

--- main.py
+++ main.py
@@ -1,52 +1,16 @@
 cook_book = {}
 
-with open('recipes.txt') as book:
-    for dish in range(1):
-        dish = book.readline().strip()
+def create_dict_from_file(file_name):
+  with open(file_name, encoding = 'utf8') as book:
+    for dish in book:
+      dish = book.readline().lower().strip()
+      num = book.readline().strip()
+      ingridient_list = []
+      cook_book.setdefault(dish, ingridient_list)
+      for ingridient in range(int(num)):
+        ingridient = book.readline().lower().strip().split(' | ')
+        ingridient = {'ingredient_name' : ingridient[0], 'quantity' : ingridient[1], 'measure' : ingridient[2]}
+        ingridient_list.append(ingridient)
 
-    for num in range(1):
-        num = book.readline().strip()
-        ingridient_list = []
-        cook_book.setdefault(dish, ingridient_list)
-        for ingridient in range(int(num)):
-            ingridient = book.readline().strip().split(' | ')
-            ingridient = {'ingredient_name': ingridient[0], 'quantity': ingridient[1], 'measure': ingridient[2]}
-            ingridient_list.append(ingridient)
-
-    for dish in range(2):
-        dish = book.readline().strip()
-
-    for num in range(1):
-        num = book.readline().strip()
-        ingridient_list = []
-        cook_book.setdefault(dish, ingridient_list)
-        for ingridient in range(int(num)):
-            ingridient = book.readline().strip().split(' | ')
-            ingridient = {'ingredient_name': ingridient[0], 'quantity': ingridient[1], 'measure': ingridient[2]}
-            ingridient_list.append(ingridient)
-
-    for dish in range(2):
-        dish = book.readline().strip()
-
-    for num in range(1):
-        num = book.readline().strip()
-        ingridient_list = []
-        cook_book.setdefault(dish, ingridient_list)
-        for ingridient in range(int(num)):
-            ingridient = book.readline().strip().split(' | ')
-            ingridient = {'ingredient_name': ingridient[0], 'quantity': ingridient[1], 'measure': ingridient[2]}
-            ingridient_list.append(ingridient)
-
-    for dish in range(2):
-        dish = book.readline().strip()
-
-    for num in range(1):
-        num = book.readline().strip()
-        ingridient_list = []
-        cook_book.setdefault(dish, ingridient_list)
-        for ingridient in range(int(num)):
-            ingridient = book.readline().strip().split(' | ')
-            ingridient = {'ingredient_name': ingridient[0], 'quantity': ingridient[1], 'measure': ingridient[2]}
-            ingridient_list.append(ingridient)
-
+create_dict_from_file('recipes.txt')
 print(cook_book)
